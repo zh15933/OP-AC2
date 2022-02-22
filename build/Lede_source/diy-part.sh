0d1b26fe0e24c772cddfe2351280232c423247f8
zh15933 <32957590+zh15933@users.noreply.github.com>
2022-02-22 22:49:22 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -12,11 +12,11 @@ git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/xhh
 
 cat >$NETIP <<-EOF
 uci set network.lan.ipaddr='192.168.5.1'                                    # IPv4 地址(openwrt后台地址)
-uci set network.lan.netmask='255.255.255.0'                                 # IPv4 子网掩码
-uci set network.lan.gateway='192.168.5.1'                                   # IPv4 网关
-uci set network.lan.broadcast='192.168.5.255'                               # IPv4 广播
-uci set network.lan.dns='223.5.5.5 114.114.114.114'                         # DNS(多个DNS要用空格分开)
-uci set network.lan.delegate='0'                                            # 去掉LAN口使用内置的 IPv6 管理
+#uci set network.lan.netmask='255.255.255.0'                                 # IPv4 子网掩码
+#uci set network.lan.gateway='192.168.5.1'                                   # IPv4 网关
+#uci set network.lan.broadcast='192.168.5.255'                               # IPv4 广播
+#uci set network.lan.dns='223.5.5.5 114.114.114.114'                         # DNS(多个DNS要用空格分开)
+#uci set network.lan.delegate='0'                                            # 去掉LAN口使用内置的 IPv6 管理
 uci commit network                                                          # 不要删除跟注释,除非上面全部删除或注释掉了
 #uci set dhcp.lan.ignore='1'                                                 # 关闭DHCP功能（去掉uci前面的#生效）
 #uci commit dhcp                                                             # 跟‘关闭DHCP功能’联动,同时启用或者删除跟注释（去掉uci前面的#生效）
