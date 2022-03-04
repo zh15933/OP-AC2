dafd5db42a8220fc8d1d117aef86cd408c4545ef
zh15933 <32957590+zh15933@users.noreply.github.com>
2022-03-04 21:38:24 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -7,8 +7,8 @@
 # 如果你的OP是当主路由的话，网关、DNS、广播都不需要，代码前面加 # 注释掉，只保留后台地址和子网掩码就可以
 # 如果你有编译ipv6的话，‘去掉LAN口使用内置的 IPv6 管理’代码前面也加 # 注释掉
 
-# 修改默认登录IP 修改192.168.5.1为你自己想要的管理IP
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+# 修改默认登录IP 修改192.168.5.5为你自己想要的管理IP
+sed -i 's/192.168.1.1/192.168.5.5/g' package/base-files/files/bin/config_generate
 
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/xhh/luci-theme-edge              # 拉取edge主题
 
